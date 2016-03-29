940a2d9ba5514b6c5ed4474b433a86f984ff76f0
lindabjalla <lindabjalla@gmail.com>
2016-03-29 19:25:04 +0200

    IssueRepository-interface byter superclass till CrudRepository från PagingAndSortingRepository.

--- project-management-jpa/src/main/java/se/grouprich/projectmanagement/repository/IssueRepository.java
+++ project-management-jpa/src/main/java/se/grouprich/projectmanagement/repository/IssueRepository.java
@@ -1,15 +1,14 @@
 package se.grouprich.projectmanagement.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
-
 import se.grouprich.projectmanagement.model.IssueData;
 import se.grouprich.projectmanagement.model.WorkItemData;
 
-public interface IssueRepository extends PagingAndSortingRepository<IssueData, Long>
+import java.util.List;
+
+public interface IssueRepository extends CrudRepository<IssueData, Long>
 {
 	@Query("SELECT i.workItem FROM #{#entityName} i")
 	List<WorkItemData> findWorkItemsHavingIssue();
