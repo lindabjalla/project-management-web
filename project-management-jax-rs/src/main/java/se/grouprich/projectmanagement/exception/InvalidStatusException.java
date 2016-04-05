4d2162b3c014300cd9e9ac32bd58f9b37e592a04
HArbietoCortez <arbietohaydee@hotmail.com>
2016-04-05 11:57:24 +0200

    Lagt till serialVersionUID...

--- project-management-jax-rs/src/main/java/se/grouprich/projectmanagement/exception/InvalidStatusException.java
+++ project-management-jax-rs/src/main/java/se/grouprich/projectmanagement/exception/InvalidStatusException.java
@@ -7,6 +7,8 @@ import static javax.ws.rs.core.Response.Status;
 
 public class InvalidStatusException extends WebApplicationException
 {
+	private static final long serialVersionUID = 1L;
+
 	public InvalidStatusException(String invalidStatusName)
 	{
 		super(Response.status(Status.BAD_REQUEST).entity("Status: " + invalidStatusName + " is invalid").build());
